bc132fc6d7f76dcd426266938fec4f935c4a0646
deepintro <deepintro@users.noreply.github.com>
2020-01-10 18:21:05 -0500

    updates

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 //document.getElementsByTagName("body")[0].style.width = window.innerWidth;
 document.getElementById("headline").style.height = window.innerHeight;
 //document.getElementById("projectsContainer").style.height = window.innerHeight;
+document.getElementById("aboutMe").style.height=window.innerHeight;
