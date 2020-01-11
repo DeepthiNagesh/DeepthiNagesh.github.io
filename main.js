148d05a0d92677fa4e4b68aa97406a966aa9cb1b
deepintro <deepintro@users.noreply.github.com>
2020-01-11 03:14:14 -0500

    lot of image updates

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 //document.getElementsByTagName("body")[0].style.width = window.innerWidth;
 document.getElementById("headline").style.height = window.innerHeight;
 //document.getElementById("projectsContainer").style.height = window.innerHeight;
-document.getElementById("aboutMe").style.height=window.innerHeight;
+setTimeout(transition, 70);
+var i = 0;
+var txt = 'I am a designer, developer and artist passionate about building beautiful, usable and accessible experiences and interfaces.'; 
+var speed = 65; 
+typeWriter();
+function transition()
+{
+	/*document.getElementById('description').style.transition='all 1s ease';*/
+	document.getElementById('name').style.transition='all 0.8s ease';
+	document.getElementById('name').style.top='10%';
+	document.getElementById('name').style.opacity=1;
+	/*document.getElementById('description').style.opacity=1;*/
+}
+
+
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("description").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
