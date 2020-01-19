0efa882be48c5124ef21a3271b92065efc5662a9
deepintro <deepintro@users.noreply.github.com>
2020-01-18 19:38:36 -0500

    made home page responsive

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var i=0;
 if(window.innerHeight>window.innerWidth)
 {
 	document.getElementById('name').style.left='10%';
+	document.getElementById('description').style.top= '10%';
+	var projectelements=document.getElementsByClassName('project-element');
+	var i=0;
+	for(i=0;i<projectelements.length;i++)
+	{
+		projectelements[i].style.width='90%';
+		projectelements[i].style.height=(projectelements[i].offsetWidth)*0.55;
+	}
 }//document.getElementById("projectsContainer").style.height = window.innerHeight;
 setTimeout(transition, 70);
 var i = 0;
